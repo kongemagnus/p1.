f237ed7169b40c1b5df272c5cd383e3386476398
kongemagnus <mklitg24@student.aau.dk>
2024-11-27 14:11:34 +0100

    help function

--- rancor.c
+++ rancor.c
@@ -1,4 +1,4 @@
 // Indlæs hvor mange koordinater du vil have
 //if statement, som kører mængden af gange du har indlæst
 // srand der opretter tilfældige koordinater hver gang if statement kører
-// Gem som array (struct pussyrassclart)
+// Gem som array (struct)
